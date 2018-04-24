ad1d3939cbeef30be8627eef1cb6df2e1159137d
ElBuenAnvita <37051987+ElBuenAnvita@users.noreply.github.com>
2018-04-24 16:45:33 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -48,10 +48,11 @@ client.on('message', message => {
 });
 
 client.on('message', message => {
-    if (message.content === 'nuf ${arg1}') {
-    	message.channel.send('${arg1} es nuf, jsjsj');
-  	}
-});
+    if (message.content === 'nuf') {
+    	message.channel.send({embed: {
+  color: 3447003,
+  description: "A very simple Embed!"
+}});
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
