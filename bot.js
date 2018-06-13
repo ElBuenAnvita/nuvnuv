43410731c47b28df550f8548f6e71d19ad619e53
ElBuenAnvita <37051987+ElBuenAnvita@users.noreply.github.com>
2018-06-12 22:01:51 -0500

    s

--- bot.js
+++ bot.js
@@ -2,64 +2,8 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = "!";
 
-client.on("message", message => {
-  if (!message.content.startsWith(prefix)) return;
-  
-  if (message.content.startsWith(prefix + "tonto")) {
-    message.channel.send(">:P\n<:tontotonto:427349831393083393> ");
-  } else
-  if (message.content.startsWith(prefix + "ayuda")) {
-    message.channel.send("Oye, soy nuv o también <la pro>, sólo puedes llamarme así.");
-  } else
-  if (message.content.startsWith(prefix + "ayuda")) {
-    message.channel.send("¡Bot desactualizado! Por favor usa la versión más reciente");
-  }
-});
-
 client.on('ready', () => {
-  client.user.setActivity('!ayuda | NuvNuv Bot')
-});
-
-client.on('message', message => {
-    if (message.content === 'nuv') {
-    	message.channel.send('Más nuv eres tú diciendo eso. Llamame, "la pro". jsjsj');
-  	}
-});
-
-client.on('message', message => {
-    if (message.content === '==help') {
-    	message.channel.send('Decí "nuv" a ver que pasa. También me pongo *sad*, ¿sabes?');
-  	}
-});
-
-client.on('message', message => {
-    if (message.content === '==corrupcion') {
-    	message.channel.send('La corrpución es mala. *Pero también buena (?*');
-  	}
-});
-
-client.on('message', message => {
-    if (message.content === 'baka') {
-    	message.channel.send('*Watashi wa bakada.*');
-  	}
-});
-
-client.on('message', message => {
-    if (message.content === 'sad') {
-    	message.channel.send('*watashi wa totemo kanashii*');
-  	}
-});
-
-client.on('message', message => {
-    if (message.content === 'la pro') {
-    	message.channel.send('ola');
-  	}
-});
-
-client.on('message', message => {
-    if (message.content === 'd mar') {
-    	message.channel.send('salada c:');
-  	}
+  client.user.setActivity('En reparaciones | Ningún comando servirá por ahora')
 });
 
 // THIS  MUST  BE  THIS  WAY
