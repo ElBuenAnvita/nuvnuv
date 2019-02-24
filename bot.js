a5e545394f5873084cd3dc62404312611c3d9234
ElBuenAnvita <37051987+ElBuenAnvita@users.noreply.github.com>
2019-02-23 23:06:05 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,29 @@ const client = new Discord.Client();
 const prefix = "!";
 
 client.on('ready', () => {
-  client.user.setActivity('En reparaciones | Ningún comando servirá por ahora')
+  client.user.setActivity('En reparaciones | Ningún comando servirá por ahora');
+  client.user.setStatus('idle'); // < eso significa que este está en prueba
+});
+
+client.on('message', message => {
+  if (message.content(prefix + "f")) {
+    var args = message.content.substring(prefix.length).split(" "); 
+    const payrespectfor = args.slice(0).join(" ");
+    if(!args[1]) {
+      embed = {
+        "title": "",
+        "description": `**${message.author.username}** ha pagado sus respetos.`,
+        "color": 2335,
+      };
+    } else {
+      embed = {
+        "title": "",
+        "description": `**${message.author.username}** ha pagado sus respetos por ${payrespectfor}`,
+        "color": 2335,
+      };
+    }
+    message.channel.send({ embed })
+  }
 });
 
 // THIS  MUST  BE  THIS  WAY
