a3be0d9f158ff8848cdfaa7bb468d5bc6a602094
ElBuenAnvita <37051987+ElBuenAnvita@users.noreply.github.com>
2019-03-22 20:46:18 -0500

    "Pronto" broadcast

--- bot.js
+++ bot.js
@@ -1,30 +1,16 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = "!";
+const prefix = "nuv!";
 
 client.on('ready', () => {
-  client.user.setActivity('En reparaciones | Ningún comando servirá por ahora');
+  client.user.setActivity(`¡Nuv volverá a funcionar en breve ;)! | ${prefix}ayuda | Ningún comando servirá por ahora`);
   client.user.setStatus('idle'); // < eso significa que este está en prueba
 });
 
 client.on('message', message => {
-  if (message.content(prefix + "f")) {
+  if (message.content(prefix + "ayuda")) {
     var args = message.content.substring(prefix.length).split(" "); 
-    const payrespectfor = args.slice(0).join(" ");
-    if(!args[1]) {
-      embed = {
-        "title": "",
-        "description": `**${message.author.username}** ha pagado sus respetos.`,
-        "color": 2335,
-      };
-    } else {
-      embed = {
-        "title": "",
-        "description": `**${message.author.username}** ha pagado sus respetos por ${payrespectfor}`,
-        "color": 2335,
-      };
-    }
-    message.channel.send({ embed })
+    message.channel.send('Pronto')
   }
 });
 
